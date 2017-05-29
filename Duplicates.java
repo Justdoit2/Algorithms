d78a6f3dd3d4c3852a26e631acb0d5d31344685a
Alma Niu <aniu@scu.edu>
2017-05-29 02:20:14 -0700

    Add files via upload

---
+++ Duplicates.java
@@ -0,0 +1,27 @@
+//Given sorted array, remove duplicates or more and return the new length
+
+import java.util.Set;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> diff= new HashSet<Integer>();
+          int k=0;
+        if(nums.length==0) return k;
+        for ( int j =0; j<nums.length;++j)
+        {
+            if(!diff.contains(nums[j])){
+            diff.add(nums[j]);
+            }
+            
+        }
+  
+        int [] A= new int[nums.length]; //prevent out of bound
+        for(Integer s: diff){
+        //System.out.println(s);
+           A[++k]=s;
+        }
+
+       return ++k;
+       
+}
+}
